858721c35c429ccf1d90d93d56e7c8d692d310c2
Benjamin <therealbenpai@gmail.com>
2024-04-29 14:26:32 -0400

    update docs

--- @types/defs.d.ts
+++ @types/defs.d.ts
@@ -3,8 +3,21 @@ import * as Express from 'express';
  * 
  */
 declare module '@therealbenpai/webutils' {
-    declare module 'Crypt' {
-        declare class CryptographyData {
+    namespace Crypt {
+        interface CDataInterface {
+            /** The initialization vector used to encrypt the data */
+            iv: string;
+            /** The key used to encrypt the data */
+            key: string;
+            /** The data that is encrypted */
+            data: string;
+            /** The data that is hashed before being encrypted */
+            prehash: string;
+            /** The data that is hashed after being encrypted */
+            posthash: string;
+        }
+        /** A Utility class used to handle the {@link Auto Crypt.Auto Class} */
+        class CryptographyData implements CDataInterface {
             iv: string;
             key: string;
             data: string;
@@ -12,37 +25,68 @@ declare module '@therealbenpai/webutils' {
             posthash: string;
             constructor();
         }
-        declare class Auto {
+        interface AutoInterface {
+            /** The function used to encrypt the data */
+            static encrypt(data: string): string;
+            /** The function used to verify the data */
+            static verify(data: string): boolean;
+            /** The function used to decrypt the data */
+            static decrypt(data: string): string;
+        }
+        class Auto implements AutoInterface {
             static encrypt(data: string): string;
             static verify(data: string): boolean;
             static decrypt(data: string): string;
         }
-        declare class Manual {
+        interface ManualInterface {
+            /** The function used to encrypt the data */
+            static encrypt(data: string): string;
+            /** The function used to verify the data */
+            static verify(data: string): boolean;
+            /** The function used to decrypt the data */
+            static decrypt(data: string): string;
+        }
+        class Manual implements ManualInterface {
             static encrypt(data: string): string;
             static verify(data: string): boolean;
             static decrypt(data: string): string;
         }
-        declare class Crypto {
+        interface CryptoInterface {
+            static Auto: Auto;
+            static Manual: Manual;
+            static completeHash(data: string): string;
+        }
+        class Crypto implements CryptoInterface {
             static Auto: Auto;
             static Manual: Manual;
             static completeHash(data: string): string;
         }
     }
-    declare module 'Dict' {
-        declare class Dict<K,V> {
-            constructor(k:K,v:V);
-            add(key: K, value: V): this;
-            get(key: K): V;
-            has(key: K): boolean;
-            remove(key: K): this
-            clear(): this;
-            addMany(...values: [K, V][]): this;
-            getMany(...keys: K[]): V[]
-            hasMany(...keys: K[]): boolean
-            removeMany(...keys: K[]): this
-        }
+    interface DictInterface<K, V> {
+        add(key: K, value: V): this;
+        get(key: K): V;
+        has(key: K): boolean;
+        remove(key: K): this;
+        clear(): this;
+        addMany(...values: [K, V][]): this;
+        getMany(...keys: K[]): V[];
+        hasMany(...keys: K[]): boolean;
+        removeMany(...keys: K[]): this;
     }
-    declare function Headers(): Express.RequestHandler;
-    declare function Logger(): Express.RequestHandler;
-    declare function Trace(): Express.RequestHandler;
+
+    class Dict<K, V> implements DictInterface<K, V> {
+        constructor(k: K, v: V);
+        add(key: K, value: V): this;
+        get(key: K): V;
+        has(key: K): boolean;
+        remove(key: K): this
+        clear(): this;
+        addMany(...values: [K, V][]): this;
+        getMany(...keys: K[]): V[]
+        hasMany(...keys: K[]): boolean
+        removeMany(...keys: K[]): this
+    }
+    function Headers(): Express.RequestHandler;
+    function Logger(): Express.RequestHandler;
+    function Trace(): Express.RequestHandler;
 }
